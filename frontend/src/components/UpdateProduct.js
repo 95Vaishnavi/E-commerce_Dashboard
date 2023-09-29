fe8e255d7598aadb523481de363551e1a5617bec
Vaishnavi Daware <vdaware166@gmail.com>
2023-09-29 23:12:00 +0530

    files uploded

---
+++ frontend/src/components/UpdateProduct.js
@@ -0,0 +1,93 @@
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const UpdateProduct = () => {
+  const [name, setName] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () => {
+    console.warn(params);
+    let result = await fetch(`http://localhost:3000/product/${params.id}`);
+    result = await result.json();
+    //console.log(result);
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
+
+  const updateProduct = async () => {
+    console.warn(name, price, category, company);
+     let result = await fetch(`http://localhost:3000/product/${params.id}`, {
+            method: 'Put',
+            body: JSON.stringify({ name, price, category, company }),
+            headers: {
+                'Content-Type': 'Application/json'
+            }
+        });
+        result = await result.json();
+        if (result) {
+            navigate('/')
+        }
+        
+  };
+
+  return (
+    <div className="product">
+      <h1>Update Product</h1>
+      <input
+        type="text"
+        placeholder="Enter product name"
+        className="InputBox"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter product price"
+        className="InputBox"
+        value={price}
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter product category"
+        className="InputBox"
+        value={category}
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter product company"
+        className="InputBox"
+        value={company}
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+      />
+
+      <button onClick={updateProduct} className="button">
+        Update Product
+      </button>
+    </div>
+  );
+};
+
+export default UpdateProduct;
